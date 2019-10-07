a0534a37b9b4ea09108793e7e5b6c29f85e81dc7
Daniel Palmer <49914604+Skermf@users.noreply.github.com>
2019-10-06 22:33:03 -0400

    Update IST412TestProject.java

--- IST412TestProject.java
+++ IST412TestProject.java
@@ -6,6 +6,8 @@ public class IST412TestProject
     public static void main(String[] args) 
     {
         Patient patient = new Patient(12345, "Bob", "Howard", 20, "9/12/1997");
+        PatientRecord patientRecord = new PatientRecord(patient);
+        
     }
     
 }
