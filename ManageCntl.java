c3ca61eaebe5395ff8b30251d3d74ce7b94c5558
Daniel Palmer <49914604+Skermf@users.noreply.github.com>
2019-10-06 22:18:21 -0400

    Update ManageCntl.java

--- ManageCntl.java
+++ ManageCntl.java
@@ -12,9 +12,8 @@ public class ManageCntl
         return;
     }
     
-    public void addRecord()
+    public void addRecord(PatientRecord patientRecord)
     {
-        
+        patientRecord = PatientRecord();
     }
 }
-
